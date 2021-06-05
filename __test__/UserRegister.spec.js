3bdcfc79f40d615a8690b6c39eef9298c1152713
yun <nasuler@gmail.com>
2021-06-05 10:47:45 +0900

    initial commit

---
+++ __test__/UserRegister.spec.js
@@ -0,0 +1,81 @@
+const request = require('supertest')
+const app = require('../src/app')
+const User = require('../src/user/User')
+const sequelize = require('../src/config/database')
+
+beforeAll(() => {
+    return sequelize.sync()
+})
+beforeEach(() => {
+    return User.destroy({ truncate: true })
+})
+describe('User Registration', () => {
+    it('returns 200 OK when signup request is valid', (done) => {
+        request(app).post('/api/1.0/users')
+            .send({
+                username: 'user1',
+                email: 'user1@mail.com',
+                password: 'P4ssword',
+            }).then((response) => {
+            expect(response.status).toBe(200)
+            done()
+        })
+    })
+
+    it('returns success message when signup request is valid', (done) => {
+        request(app).post('/api/1.0/users')
+            .send({
+                username: 'user1',
+                email: 'user1@mail.com',
+                password: 'P4ssword',
+            }).then((response) => {
+            expect(response.body.message).toBe('User Created')
+            done()
+        })
+    })
+
+    it('saves the user to databse', (done) => {
+        request(app).post('/api/1.0/users')
+            .send({
+                username: 'user1',
+                email: 'user1@mail.com',
+                password: 'P4ssword',
+            }).then(() => {
+            User.findAll().then((userList) => {
+                expect(userList.length).toBe(1)
+                done()
+            })
+        })
+    })
+
+    it('saves the username and email to database', (done) => {
+        request(app).post('/api/1.0/users')
+            .send({
+                username: 'user1',
+                email: 'user1@mail.com',
+                password: 'P4ssword',
+            }).then(() => {
+            User.findAll().then((userList) => {
+                const savedUser = userList[0]
+                expect(savedUser.username).toBe('user1')
+                expect(savedUser.email).toBe('user1@mail.com')
+                done()
+            })
+        })
+    })
+
+    it('hashes the password in database', (done) => {
+        request(app).post('/api/1.0/users')
+            .send({
+                username: 'user1',
+                email: 'user1@mail.com',
+                password: 'P4ssword',
+            }).then(() => {
+            User.findAll().then((userList) => {
+                const savedUser = userList[0]
+                expect(savedUser.password).not.toBe('P4ssword')
+                done()
+            })
+        })
+    })
+})
---
+++ src/app.js
@@ -0,0 +1,13 @@
+const express = require('express')
+const User = require('./user/User')
+const app = express()
+
+app.use(express.json())
+
+app.post('/api/1.0/users', (req, res)=>{
+    User.create(req.body).then(()=>{
+        return res.send({message: 'User Created'})
+    })
+})
+
+module.exports = app;
