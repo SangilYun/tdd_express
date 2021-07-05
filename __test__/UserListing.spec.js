816fae417251cf1b1816c8b867f665a716c7fcdd
yun <nasuler@gmail.com>
2021-07-05 09:04:19 +0900

    Add Get user tests and error translations

--- __test__/UserListing.spec.js
+++ __test__/UserListing.spec.js
@@ -54,6 +54,92 @@ describe('Listing Users', () => {
         await addUsers(11)
         const response = await getUsers()
         const user = response.body.content[0]
-        expect(Object.keys(user)).toEqual(['id', 'username', 'email'])
+        expect(Object.keys(user)).toEqual([ 'id', 'username', 'email' ])
+    })
+    it('returns 2 as totalPages when there are 15 active and 8 inactive users', async function () {
+        await addUsers(15, 7)
+        const response = await getUsers()
+        expect(response.body.totalPages).toBe(2)
+    });
+    it('returns second page users and page indicator when page is set as 1 in request parameter', async function () {
+        await addUsers(11)
+        const response = await getUsers().query({ page: 1 })
+        expect(response.body.content[0].username).toBe('user11')
+        expect(response.body.page).toBe(1)
+    });
+    it('returns first page when page is set below zero as request parameter', async function () {
+        await addUsers(11)
+        const response = await getUsers().query({ page: -5 })
+        expect(response.body.page).toBe(0)
+    });
+    it('returns 5 users and corresponding size indicator when size is set as 5 in request parameter', async function () {
+        await addUsers(11)
+        const response = await getUsers().query({ size: 5 })
+        expect(response.body.content.length).toBe(5)
+        expect(response.body.size).toBe(5)
+    });
+    it('returns 10 users and corresponding size indicator when size is set as 1000', async function () {
+        await addUsers(11)
+        const response = await getUsers().query({ size: 1000 })
+        expect(response.body.content.length).toBe(10)
+        expect(response.body.size).toBe(10)
+    });
+    it('returns 10 users and corresponding size indicator when size is set as 0', async function () {
+        await addUsers(11)
+        const response = await getUsers().query({ size: 0 })
+        expect(response.body.content.length).toBe(10)
+        expect(response.body.size).toBe(10)
+    });
+    it('returns page as zero and size as 10 when non numeric query params provided for both', async function () {
+        await addUsers(11)
+        const response = await getUsers().query({ size: 'size', page: 'page' })
+        expect(response.body.size).toBe(10)
+        expect(response.body.page).toBe(0)
+    });
+});
+describe('Get user', () => {
+    const getUser = (id = 5 ) => {
+        return request(app).get('/api/1.0/users/' + id)
+    }
+    it('returns 404 when user not found', async () => {
+        const response = await getUser()
+        expect(response.status).toBe(404)
+    })
+    it.each`
+    language | message
+    ${ 'ko' }  | ${ 'User not found in other languages' }
+    ${ 'en' }  | ${ 'User not found' }
+    `('returns $message for unknown user when language is set to $language', async ({ language, message }) => {
+        const response = await getUser().set('Accept-Language', language)
+        expect(response.body.message).toBe(message)
+    })
+    it('returns proper error body when user not found', async () => {
+        const nowInMillis = new Date().getTime()
+        const response = await getUser()
+        const error = response.body
+        expect(error.path).toBe('/api/1.0/users/5')
+        expect(error.timeStamp).toBeGreaterThan(nowInMillis)
+        expect(Object.keys(error)).toEqual([ 'message', 'path', 'timeStamp' ])
+    })
+    it('returns 200 when an active user exist', async () => {
+        const user = await User.create({
+            username: 'user1', email: 'user1@mail.com', inactive: false
+        })
+        const response = await getUser(user.id)
+        expect(response.status).toBe(200)
+    })
+    it('returns id, username and email in response body when an active user exists', async () => {
+        const user = await User.create({
+            username: 'user1', email: 'user1@mail.com', inactive: false
+        })
+        const response = await getUser(user.id)
+        expect(Object.keys(response.body)).toEqual(['id', 'username', 'email'])
+    })
+    it('returns 404 when the user is inactive', async () => {
+        const user = await User.create({
+            username: 'user1', email: 'user1@mail.com', inactive: true
+        })
+        const response = await getUser(user.id)
+        expect(response.status).toBe(404)
     })
 });
