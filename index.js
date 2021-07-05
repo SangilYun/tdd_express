03f7233dd42de7f249bd7b3f461a797ee48ccbfb
yun <nasuler@gmail.com>
2021-07-05 09:03:03 +0900

    Add mock users for manual testing

--- index.js
+++ index.js
@@ -1,5 +1,20 @@
 const app = require('./src/app')
 const sequelize = require('./src/config/database')
+const User = require('./src/user/User')
 
-sequelize.sync({ force: true })
+const addUsers = async (activeUserCount, inactiveUserCount = 0) => {
+    const count = activeUserCount + inactiveUserCount
+    for (let i = 0; i < count; i++) {
+        await User.create({
+            username: `user${ i + 1 }`,
+            email: `user${ i + 1 }@mail.com`,
+            inactive: i >= activeUserCount
+        })
+    }
+}
+
+sequelize.sync({ force: true }).then(async () => {
+    await addUsers(25)
+})
+
 app.listen(3000, () => console.log('app is running'))
