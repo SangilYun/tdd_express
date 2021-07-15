8616f72da763562e416576e62293164d3217bd6d
yun <nasuler@gmail.com>
2021-07-15 10:12:49 +0900

    Implement Authentication Error

--- src/auth/AuthenticationRouter.js
+++ src/auth/AuthenticationRouter.js
@@ -2,9 +2,13 @@ const express = require('express')
 const router = express.Router()
 
 const UserService = require('../user/UserService')
+const AuthenticationException = require('./AuthenticationException')
 
-router.post('/api/1.0/auth', async (req, res) => {
+router.post('/api/1.0/auth', async (req, res, next) => {
     const user = await UserService.findByEmail(req.body.email)
+    if(!user){
+        return next(new AuthenticationException())
+    }
     res.send({
         id: user.id,
         username: user.username
