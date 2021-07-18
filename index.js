43fce7910ae7ce6dadc8bb5fe5a314ac04f69fc5
yun <nasuler@gmail.com>
2021-07-18 15:34:23 +0900

    add password to the mock user data before implementing authorization

--- index.js
+++ index.js
@@ -1,14 +1,17 @@
 const app = require('./src/app')
 const sequelize = require('./src/config/database')
 const User = require('./src/user/User')
+const bcrypt = require('bcrypt')
 
 const addUsers = async (activeUserCount, inactiveUserCount = 0) => {
+    const hash = await bcrypt.hash('P4ssword', 10)
     const count = activeUserCount + inactiveUserCount
     for (let i = 0; i < count; i++) {
         await User.create({
             username: `user${ i + 1 }`,
             email: `user${ i + 1 }@mail.com`,
-            inactive: i >= activeUserCount
+            inactive: i >= activeUserCount,
+            password: hash
         })
     }
 }
